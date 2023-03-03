943e879f2e5794aa8dbe946fe613a64ce04dbf3e
gyuheeb <gyuhee4529@naver.com>
2023-03-03 14:49:40 +0900

    BE #1

---
+++ helloweb-ex01/app01.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var port= 9090;
+var server = http.createServer(function(req, resp){
+    resp.writeHead(200, {
+        'Content-Type':"text/html"
+
+    });
+    resp.end('<h1> Hello World </h1>');
+
+});
+
+server.listen(port, function(){
+    console.log('http server runing on '+port);
+});

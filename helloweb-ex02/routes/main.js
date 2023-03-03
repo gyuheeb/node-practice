855addf1aa3b52737b5583e75f77a7ac30b561cd
gyuheeb <gyuhee4529@naver.com>
2023-03-03 16:08:25 +0900

    node express tutorial

---
+++ helloweb-ex02/routes/main.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.route('').get(function(req,resp){
+    resp.render('main/index');
+});
+
+
+
+
+
+
+
+module.exports =  router;
